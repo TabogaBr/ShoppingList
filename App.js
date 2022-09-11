27872171e176eaf7845dccae679d03a1623cbfa4
TabogaBr <brenda.taboga7@gmail.com>
2022-09-11 18:26:17 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [data, setData] = useState([]);
+  const [text, setText] = useState('');
+
+  const add = () => {
+    setData([...data, {key: text}]);
+    setText('');
+  }
+
+  const clear = () => {
+    setData([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style = {[styles.textinputs, styles.margins, styles.textTop]}
+      onChangeText={text => setText(text)}
+      value={text}/>
+      <View style = {styles.buttons} >
+        <Button onPress = {add} title = " ADD " />
+        <Button onPress = {clear} title = " CLEAR " />
+      </View>
+      <FlatList contentContainerStyle = {styles.items}
+      style = {styles.list}
+      data = {data}
+      renderItem = {({item}) => <Text style = {styles.item} > {item.key} </Text>}
+      ListHeaderComponent = {<Text style = {styles.title} > Shopping List </Text>}
+      />
     </View>
   );
 }
@@ -15,6 +39,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  textinputs: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+  margins: {
+    margin: '3%',
+  },
+  list: {
+    paddingVertical: '5%',
+  },
+  textTop: {
+    marginTop: 100,
+  },
+  items: {
+    alignItems: 'center',
+  },
+  title: {
+    color: 'blue',
+    fontSize: 18,
+  },
+  item: {
+    fontSize: 16,
   },
 });
